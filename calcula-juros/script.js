f1bccad3b132a0e7c9d4e34bfc7ce0177dd3d8e9
CRISTINA SARAIVA FIGUEIREDO <kristinasaraiva12@gmail.com>
2025-04-18 14:55:43 -0300

    recuperando sub-pastas

---
+++ calcula-juros/script.js
@@ -0,0 +1,18 @@
+function calcularJuros() {
+    var nome = document.getElementById('nome').value;
+    var capitalInicial = parseFloat(document.getElementById('capitalInicial').value);
+    var valorAcrescimo = parseFloat(document.getElementById('acrescimo').value);
+    var res = document.getElementById('res');
+
+    if (!nome || isNaN(capitalInicial) || isNaN(valorAcrescimo)) { alert('Por favor, preencha todos os campos corretamente.'); return;
+    }
+
+    var juros = (valorAcrescimo / capitalInicial) * 100; res.innerHTML = `Cliente: ${nome} <br> Taxa de Juros: ${juros.toFixed(2)}%`;
+}
+
+function limparFormulario() {
+    document.getElementById('nome').value = '';
+    document.getElementById('capitalInicial').value = '';
+    document.getElementById('acrescimo').value = '';
+    document.getElementById('res').innerHTML = '';
+}
