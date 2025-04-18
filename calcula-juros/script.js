92d45589acccb6668276369cae5f5b95f5c854fe
CRISTINA SARAIVA FIGUEIREDO <kristinasaraiva12@gmail.com>
2025-04-18 14:05:28 -0300

    mudanças de calculo no javascript e no input html

--- calcula-juros/script.js
+++ calcula-juros/script.js
@@ -1,11 +1,11 @@
 function calcularJuros() {
     var nome = document.getElementById('nome').value;
     var capitalInicial = parseFloat(document.getElementById('capitalInicial').value);
-    var capitalDevolvido = parseFloat(document.getElementById('capitalDevolvido').value);
+    var valorAcrescimo = parseFloat(document.getElementById('acrescimo').value);
     var res = document.getElementById('res');
 
-    if (!nome || isNaN(capitalInicial) || isNaN(capitalDevolvido)) { alert('Por favor, preencha todos os campos corretamente.'); return;
+    if (!nome || isNaN(capitalInicial) || isNaN(valorAcrescimo)) { alert('Por favor, preencha todos os campos corretamente.'); return;
     }
 
-    var juros = ((capitalDevolvido - capitalInicial) / capitalInicial) * 100; res.innerHTML = `Cliente: ${nome} <br> Taxa de Juros: ${juros.toFixed(2)}%`;
+    var juros = (valorAcrescimo / capitalInicial) * 100; res.innerHTML = `Cliente: ${nome} <br> Taxa de Juros: ${juros.toFixed(2)}%`;
 }
